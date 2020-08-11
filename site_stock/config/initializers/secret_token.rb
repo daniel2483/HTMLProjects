c28d74051e999be5c8fda4821f1b5f6449e771e8
J Daniel R <52753172+daniel2483@users.noreply.github.com>
2020-08-11 11:00:14 -0600

    config files

---
+++ site_stock/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteStock::Application.config.secret_token = '1e48591938c23575b3346882153d2736ca44ff709d619198760942bd996a16e7ca40f82992d68b776ff0c61eeef31ad1e6e7f92cee7971b94656b016dcda0d37'
